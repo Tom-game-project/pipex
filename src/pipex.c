38c7ee38d453d6227201b353cc59480c8936e629
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2024-11-09 17:57:19 +0900

    delete `cmd_n`

--- src/pipex.c
+++ src/pipex.c
@@ -21,24 +21,14 @@
 // for test
 #include <stdio.h>
 
-/*
-char *cmd1[] = {"cat", NULL};
-char *cmd2[] = {"head", NULL};
-char *cmd3[] = {"grep", "char", NULL};
-char **cmds[] = {cmd1, cmd2, cmd3};
-int cmd_n = 3;
-*/
 
 int	main(int argc, char *argv[], char *envp[])
 {
 	char ***cmds;
 	pid_t pid;
 	t_input *inp;
-	int cmd_n;
 	int exit_status;
 	int status;
-
-	(void) envp;
 
 	exit_status = 0;
 	inp = (t_input*) malloc(sizeof(t_input));
@@ -46,15 +36,14 @@ int	main(int argc, char *argv[], char *envp[])
 		return (1);
 	inp -> infile = argv[1];
 	cmds = get_cmds(argc, argv);
-	cmd_n = argc - 2 - 1;
 	inp -> cmds = cmds;
-	inp -> cmdlen = cmd_n;
+	inp -> cmdlen =  argc - 2 - 1;
 	inp -> outfile = argv[argc - 1];
 	// printf("infile %s \n",inp -> infile);
 	// fork
 	pid = fork(); // if error pid == -1
 	if (pid == 0) // if child
-		 run_pipes(cmd_n - 1, inp, envp);
+		 run_pipes((argc - 2 - 1) - 1, inp, envp);
 	else if (pid == -1)
 	{
 		// pass;
