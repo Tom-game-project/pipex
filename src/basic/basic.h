52e5fdfa87373f73194492703c206c3b45f59188
Tom0427 <82128211+Tom-game-project@users.noreply.github.com>
2025-01-10 19:58:52 +0900

    [FIX] fix norminette

--- src/basic/basic.h
+++ src/basic/basic.h
@@ -6,20 +6,20 @@
 /*   By: tmuranak <tmuranak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 19:30:44 by tmuranak          #+#    #+#             */
-/*   Updated: 2025/01/09 19:08:35 by tmuranak         ###   ########.fr       */
+/*   Updated: 2025/01/10 19:56:18 by tmuranak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BASIC_H
 # define BASIC_H
 
-#include <stdbool.h>
+# include <stdbool.h>
 
 char	*copy_string(char *start, char *end);
 int		ft_strlen(char *str);
 char	*ft_strjoin(char const *s1, char const *s2);
 int		startswith(char *src, char *target);
-bool ft_streq(char *a, char *b);
+bool	ft_streq(char *a, char *b);
 char	*ft_strclone(char *str);
 
 #endif
--- src/basic/basic01.c
+++ src/basic/basic01.c
@@ -1,13 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   basic01.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmuranak <tmuranak@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/10 19:55:30 by tmuranak          #+#    #+#             */
+/*   Updated: 2025/01/10 19:56:53 by tmuranak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "basic.h"
 #include <stdbool.h>
 
 /// 同じ文字列かどうかを判定する
-bool ft_streq(char *a, char *b)
+bool	ft_streq(char *a, char *b)
 {
-	
 	if (ft_strlen(a) != ft_strlen(b))
 		return (false);
-	else 
+	else
 	{
 		while (*a != '\0')
 		{
@@ -19,4 +30,3 @@ bool ft_streq(char *a, char *b)
 		return (true);
 	}
 }
-
